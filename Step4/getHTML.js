54780ac7f990eac1e1cf36e36cedd826b3b4e6de
Vasiliy Klimkin <vasily.klimkin@gmail.com>
2017-06-05 17:07:42 +0000

    Finished step 4

---
+++ Step4/getHTML.js
@@ -0,0 +1,28 @@
+function getHTML (options, callback) {
+  var https = require("https");
+  /* Add your code here */
+  let buffer = "";
+  https.get(options, function(response) {
+    response.setEncoding("utf8");
+    response.on("data", function(data) {
+      buffer += data;
+    });
+
+    response.on("end", function() {
+      callback(buffer);
+    });
+  });
+
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions,printHTML);
